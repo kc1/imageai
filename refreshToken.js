a0d099fe826314e8a4d710a9a64bd75d7747040c
Ubuntu <ubuntu@penguin.lxd>
2025-01-04 21:29:08 -0500

    first commit

---
+++ refreshToken.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+
+async function refreshDropboxToken() {
+    const params = new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: 'UOMpkiFcvhYAAAAAAAAAAUewQset5jLmSXhR_WhteKBN7CBO8Yvb4F7_TCCe9Nyr',
+        client_id: '4wlwoffttm98qno',
+        client_secret: '6xe8cx18dgq5oa5'
+    });
+
+    try {
+        const response = await fetch('https://api.dropbox.com/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: params
+        });
+
+        const data = await response.json();
+        console.log('New access token:', data.access_token);
+        return data;
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+        throw error;
+    }
+}
+
+refreshDropboxToken();
