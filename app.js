2caecf44d9e4a611de8acf78eea879840f4d85e1
Ubuntu <ubuntu@penguin.lxd>
2025-01-12 20:37:28 -0500

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const sharp = require("sharp");
 
 const { launchBrowser } = require("./stealthPlaywright");
-const { performTest } = require('./tests/test-5.spec.ts');
+const { performTest } = require("./tests/test-5.spec.ts");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,20 +10,62 @@ const port = process.env.PORT || 3000;
 // Add JSON middleware
 app.use(express.json());
 
-app.post("/process", async (req, res) => {
+app.post("/processMany", async (req, res) => {
   try {
-    const { property } = req.body;
-    
-    if (!property || !property.state || !property.county || !property.apn) {
-      return res.status(400).json({ error: 'Missing property details' });
-    }
-    console.log('Property:', property);
+    const properties = req.body;
+    console.log("Properties:", properties);
+
+    if (!properties || !properties.length) return "No properties to process";
+
+    // return properties;
 
     const browser = await launchBrowser();
     const context = await browser.newContext({
       permissions: ["geolocation"],
       geolocation: {
-        latitude: 45.680386849221 ,
+        latitude: 45.680386849221,
+        longitude: -90.361372973983,
+      },
+      javaScriptEnabled: true,
+    });
+
+    for (let i = 0; i < properties.length; i++) {
+      let property = properties[i];
+      property.apn = property.APN;
+      if (property && property.state && property.county && property.APN) {
+        const page = await context.newPage();
+        await performTest(page, property);
+      }
+    }
+
+    await browser.close();
+
+    res.json({
+      success: true,
+      data: {
+        properties,
+        message: "Processing complete",
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/process", async (req, res) => {
+  try {
+    const { property } = req.body;
+
+    if (!property || !property.state || !property.county || !property.apn) {
+      return res.status(400).json({ error: "Missing property details" });
+    }
+    console.log("Property:", property);
+
+    const browser = await launchBrowser();
+    const context = await browser.newContext({
+      permissions: ["geolocation"],
+      geolocation: {
+        latitude: 45.680386849221,
         longitude: -90.361372973983,
       },
       javaScriptEnabled: true,
@@ -34,12 +76,12 @@ app.post("/process", async (req, res) => {
     await performTest(page, property);
 
     await browser.close();
-    res.json({ 
-      success: true, 
+    res.json({
+      success: true,
       data: {
         property,
-        message: 'Processing complete'
-      }
+        message: "Processing complete",
+      },
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
