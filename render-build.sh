388dc88e47d0f791ba4fa1bda13e57869e73df58
Ubuntu <ubuntu@penguin.lxd>
2025-01-19 23:09:01 -0500

    first commit

--- render-build.sh
+++ render-build.sh
@@ -6,11 +6,13 @@ export PLAYWRIGHT_BROWSERS_PATH="/opt/render/project/playwright"
 
 npm install                                                               ║
 # npx playwright install    
-npx playwright install
+# npx playwright install
+npx playwright install-deps chromium
+
+
 # yarn playwright install chromium
 # Install Playwright browsers
 
-# npm install                                                               ║
 # Store/pull Playwright cache with build cache
 if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
   echo "...Copying Playwright Cache from Build Cache"  
