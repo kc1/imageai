69e858e6d196a2f8d53d4ec7478b2908b4b6bea4
Ubuntu <ubuntu@penguin.lxd>
2025-01-23 20:10:54 -0500

    first commit

--- render-build.sh
+++ render-build.sh
@@ -4,7 +4,7 @@ set -e
 # Set the PLAYWRIGHT_BROWSERS_PATH environment variable
 export PLAYWRIGHT_BROWSERS_PATH="/opt/render/project/playwright"
 
-npm install                                                               ║
+npm install                                   
 npx playwright install-deps chromium
 # yarn playwright install chromium
 
