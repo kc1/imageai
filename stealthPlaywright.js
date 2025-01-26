17bd97a26facaf09f62c09e0eda4cf016ae06053
Ubuntu <ubuntu@penguin.lxd>
2025-01-26 12:28:21 -0500

    first commit

--- stealthPlaywright.js
+++ stealthPlaywright.js
@@ -5,7 +5,8 @@ const sharp = require("sharp");
 chromium.use(stealth);
 
 async function launchBrowser(headless = true) {
-    const browser = await chromium.launch({ headless: headless });
+    // const browser = await chromium.launch({ headless: headless });
+    const browser = await playwright.chromium.connect(process.env.BROWSER_PLAYWRIGHT_ENDPOINT);
 
     return browser;
 }
