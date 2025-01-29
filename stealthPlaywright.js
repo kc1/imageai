7e2eb13b8dfc1db9f741a40fc3603640d877fcc4
Ubuntu <ubuntu@penguin.lxd>
2025-01-28 22:13:56 -0500

    first commit

--- stealthPlaywright.js
+++ stealthPlaywright.js
@@ -8,7 +8,7 @@ async function launchBrowser() {
   const browser = await chromium.launch({
     headless: true,
     proxy: {
-      server: 'http://23.94.138.75:6349', // Replace with your proxy server
+      server: 'http://198.23.239.134:6540', // Replace with your proxy server
       username: 'asdyycvx', // Optional, if your proxy requires authentication
       password: 'qk597lgwe2ni'  // Optional, if your proxy requires authentication
     }
