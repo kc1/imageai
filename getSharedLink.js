337c59fbb7ee42e0b9b813f59c5a0a5546937b59
Ubuntu <ubuntu@penguin.lxd>
2025-05-26 16:39:40 -0400

    first commit

--- getSharedLink.js
+++ getSharedLink.js
@@ -3,7 +3,9 @@ async function getSharedLink(dbx, filePath) {
     const response = await dbx.sharingCreateSharedLinkWithSettings({
       path: filePath,
     });
-    return response.result.url;
+    let url = response.result.url.replace('dl=0', 'raw=1');
+    return url;
+
   } catch (error) {
     console.error("Error creating shared link for", filePath, error);
     throw error;
