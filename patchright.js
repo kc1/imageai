45889ee85e0dd0b99962915acc7ed377bdcb2edf
Ubuntu <ubuntu@penguin.lxd>
2025-10-18 18:45:36 -0400

    first commit

--- patchright.js
+++ patchright.js
@@ -2,15 +2,15 @@ const { chromium } = require('patchright');
 
 async function launchBrowser() {
   // Use system Chromium on Render, fallback to Playwright's Chromium locally
-  const executablePath = process.env.NODE_ENV === 'production' 
-    ? '/usr/bin/chromium' 
-    : '/opt/render/project/.cache/playwright/chromium-1194/chrome-linux/chrome';
+  // const executablePath = process.env.NODE_ENV === 'production' 
+    // ? '/usr/bin/chromium' 
+    // : '/opt/render/project/.cache/playwright/chromium-1194/chrome-linux/chrome';
     
   const browser = await chromium.launch({
     // args: ["--use-angle=gl"],
     args: ["--enable-unsafe-swiftshader"],
-    headless: true,
-    executablePath: executablePath
+    headless:false ,
+    // executablePath: executablePath
      });
   return browser;
 }
